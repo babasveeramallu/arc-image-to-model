e172eeea93644ee5be0e8bf540b9293f533f82bf
babasveeramallu <baba.s.veeramallu@gmail.com>
2025-11-25 21:53:54 -0600

    Use minimal Replit configuration

--- replit.nix
+++ replit.nix
@@ -1,34 +1,6 @@
 { pkgs }: {
   deps = [
     pkgs.python311Full
-    pkgs.replitPackages.prybar-python311
-    pkgs.replitPackages.stderred
-    pkgs.pkg-config
-    pkgs.libffi
-    pkgs.openssl
-    pkgs.gcc
-    pkgs.zlib
-    pkgs.libjpeg
-    pkgs.libpng
-    pkgs.freetype
+    pkgs.pip
   ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      pkgs.glib
-      pkgs.xorg.libX11
-      pkgs.libffi
-      pkgs.openssl
-      pkgs.libjpeg
-      pkgs.libpng
-      pkgs.freetype
-    ];
-    PYTHONHOME = "${pkgs.python311Full}";
-    PYTHONBIN = "${pkgs.python311Full}/bin/python3.11";
-    LANG = "en_US.UTF-8";
-    STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python311}/bin/prybar-python311";
-    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.libffi.dev}/lib/pkgconfig";
-  };
 }
