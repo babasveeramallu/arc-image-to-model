da20c1ab9a616c597e69aad0036b6d104feb76a8
babasveeramallu <baba.s.veeramallu@gmail.com>
2025-11-25 21:51:42 -0600

    Fix Replit configuration and add backup files

--- replit.nix
+++ replit.nix
@@ -1,23 +1,34 @@
 { pkgs }: {
   deps = [
-    pkgs.python310Full
-    pkgs.replitPackages.prybar-python310
+    pkgs.python311Full
+    pkgs.replitPackages.prybar-python311
     pkgs.replitPackages.stderred
+    pkgs.pkg-config
+    pkgs.libffi
+    pkgs.openssl
+    pkgs.gcc
+    pkgs.zlib
+    pkgs.libjpeg
+    pkgs.libpng
+    pkgs.freetype
   ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Needed for pandas / numpy
       pkgs.stdenv.cc.cc.lib
       pkgs.zlib
-      # Needed for pygame
       pkgs.glib
-      # Needed for matplotlib
       pkgs.xorg.libX11
+      pkgs.libffi
+      pkgs.openssl
+      pkgs.libjpeg
+      pkgs.libpng
+      pkgs.freetype
     ];
-    PYTHONHOME = "${pkgs.python310Full}";
-    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
+    PYTHONHOME = "${pkgs.python311Full}";
+    PYTHONBIN = "${pkgs.python311Full}/bin/python3.11";
     LANG = "en_US.UTF-8";
     STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
+    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python311}/bin/prybar-python311";
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.libffi.dev}/lib/pkgconfig";
   };
 }
